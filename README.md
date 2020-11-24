0308a3eedd659294807cd84521938627fad6ada9
jos <soydesilla@hotmail.es>
2020-11-24 19:55:28 +0100

    sinc tablas

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-dbSYNC
+# Sincronizar mySql
+Sincroniza dos bases de datos mySql. Sincronizando tablas o bases de datos enteras.
+
+## Sincronizar tablas
+Sincroniza dos tablas de un host a otro.
+```php
+
+include(__DIR__.'/dbSYNC.php');
+
+$sync = new dbSync(
+    array(
+        'user'  =>'',
+        'passwd'=>'',
+        'host'  =>'',
+        'db'    =>'',
+        'port'  =>''
+    ),
+    array(
+        'user'  =>'',
+        'passwd'=>'',
+        'host'  =>'',
+        'db'    =>'',
+        'port'  =>''
+    )
+);
+
+$sync->sincronizar_tabla('TABLA');
+
+
+```
--- src/dbSYNC.php
+++ src/dbSYNC.php
@@ -7,12 +7,61 @@ class dbSync{
 
     public function __construct($db1,$db2){
 
-        $this->$db1 = new db($db1['user'],$db1['passwd'],$db1['host'],$db1['db'],$db1['port']);
-        $this->$db2 = new db($db2['user'],$db2['passwd'],$db2['host'],$db2['db'],$db2['port']);
+        $this->db1 = new db($db1['user'],$db1['passwd'],$db1['host'],$db1['db'],$db1['port']);
+        $this->db2 = new db($db2['user'],$db2['passwd'],$db2['host'],$db2['db'],$db2['port']);
 
     }
 
-    public function sincronizar_tabla($tabla){
+    /**
+     * Sincroniza los datos de la tabla ( si la tabla ya existe con la misma estructura )
+     */
+    public function sincronizar_tabla($tabla,$reset=false){
+
+        if($reset){
+            //Borrar datos de la tabla.
+            $this->db2->_db_consulta('TRUNCATE TABLE '.$tabla);
+        }
+
+        $tablaMaestra = $this->db1->_db_consulta(
+            "SELECT * FROM ".$tabla
+        );
+
+        foreach($tablaMaestra->rows as $fila){
+
+            $exist=false;
+
+            //Comprobar que no existe el registro.
+            if(!$reset){
+
+                $where =" ";
+                $columnas = array_keys($fila);
+                for($i=0;$i<count($columnas);$i++){
+
+                    if($where !=" "){
+                        $where .= " AND ";
+                    }
+
+                    //Añadimos el parametro.
+                    $where .= $columnas[$i]."='".$fila[$columnas[$i]]."'";
+
+                }
+                
+                $if = "SELECT * FROM ".$tabla." WHERE ".$where;
+                if($this->db2->_db_consulta($if)->num_rows >0){
+                    $exist=true;
+                }
+
+            }
+            // ./Comprobar que no existe el registro.
+
+            if($exist==false){
+                //Insertamos la fila.
+                $sql="INSERT INTO ".$tabla." (".implode(", ",array_keys($fila)).") VALUES ('".implode("', '",array_values($row))."')";
+                //Lanzar peticion.
+                $this->db2->_db_consulta($sql);
+            }
+
+        }
 
     }
 
